c057c1c42ab7f46c8aa108df0880af141ce4d345
Andrewel <vlason15@gmail.com>
2018-11-19 13:29:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -48,8 +48,8 @@ server.post('/getQuotes',function (request,response)  {
                 }
                     response.setHeader('Content-Type', 'application/json');
                     response.send(JSON.stringify({
-                        "speech" : res.body.quote.body + "\n--" + res.body.quote.author,
-                        "displayText" : res.body.quote.body + "\n--" + res.body.quote.author
+                        "fulfillmentText" : res.body.quote.body + "\n--" + res.body.quote.author
+                       // "displayText" : res.body.quote.body + "\n--" + res.body.quote.author
                     }));
 
             });
